fd92b64776fe101149b8fea31bdf3568037be847
Adrian88 <adrian88szymanski@gmail.com>
2022-01-14 10:14:29 +0100

    1 change. letter only support

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -11,6 +11,12 @@ enc_msg = ''
 for i in message:
     number = ord(i)
     number += key
+    if i.isupper():
+        if number > 90:
+            number -= 26
+    else:
+        if number > 122:
+            number -= 26
     enc_msg += chr(number)
 
 print("Your encrypted message is:", enc_msg)
