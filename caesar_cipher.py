890ee70958569d7cf6b83c602968ba85e21a2ab7
Adrian88 <adrian88szymanski@gmail.com>
2022-01-14 10:25:45 +0100

    3 change. create def caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 
   # caesar cipher
-  # Message to cipher
+
+
+def caesar_cipher(message, key):
+    # variable storing the encrypted message
+    enc_msg = ''
+
+    for i in message:
+        if i.isalpha():
+            number = ord(i)
+            number += key
+            if i.isupper():
+                if number > 90:
+                    number -= 26
+            else:
+                if number > 122:
+                    number -= 26
+            enc_msg += chr(number)
+        else:
+            enc_msg += i
+    return enc_msg
+
 print("Give me your message.")
 message = input()
 print("Give me your key.")
 key = int(input())
-  # variable storing the encrypted message
-enc_msg = ''
-for i in message:
-    if i.isalpha():
-        number = ord(i)
-        number += key
-        if i.isupper():
-            if number > 90:
-                number -= 26
-        else:
-            if number > 122:
-                number -= 26
-        enc_msg += chr(number)
-    else:
-        enc_msg += i
 
-print("Your encrypted message is:", enc_msg)
+print("Your encrypted message is:")
+print(caesar_cipher(message, key))
