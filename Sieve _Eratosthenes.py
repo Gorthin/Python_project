e3a7a585651f7ae256d8c2d1c7bc119ccd9d59f5
Adrian88 <adrian88szymanski@gmail.com>
2022-01-16 08:34:12 +0100

    2 change. Display list with primes number

--- Sieve _Eratosthenes.py
+++ Sieve _Eratosthenes.py
@@ -10,15 +10,16 @@
 
 def sieve(n):
     temp = [1 for x in range(n+1)]
-    for i in range(2,n):
-        for j in range(i+1, n):
+    for i in range(2,n+1):
+        for j in range(i+1, n+1):
             if j % i == 0:
                 temp[j] = 0
+
     primes = []
-    for i in range(len(temp)):
-        if temp[i] == 0:
+    for i in range(2,len(temp)):
+        if temp[i] == 1:
             primes.append(i)
 
     return primes
 
-print(sieve(30))
+print(sieve(100))
